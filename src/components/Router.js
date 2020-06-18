880f918450878090ebb6dd87fd839fe8b84dd711
Andres Ponciano <andres.ponciano123@gmail.com>
2020-06-18 16:22:01 -0400

    video player working now

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from './App';
+import videoPlayer from './videoPlayer';
 
 function Router () {
     return (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={App}/>
-            {/* <Route path="/:video" component={}/> */}
+            <Route path="/:video" component={videoPlayer}/>
         </Switch>
     </BrowserRouter>
     );
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
-class videoPlayer extends Component {
+class videoPlayer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        console.log(this.props.location.state.video.videoData.id.videoId);
+
+    }
+
+
     render() {
+
+        // const video = this.props.location.state.video;
+
+        // console.log('video is', video);
+        // const url = 'https://www.youtube.com/embed/' + video.id.videoId;
+        // console.log('url is', url);
+
         return(
             <>
             <h1>im in video player</h1>
+                {/* {this.props.location.state.video.id.videoId} */}
+                <iframe src={`https://www.youtube.com/embed/${this.props.location.state.video.videoData.id.videoId}`}
+                    frameBorder='0'
+                    allow='autoplay; encrypted-media'
+                    allowFullScreen
+                    title='video'
+                />
             </>
         )
     }
 }
+
+export default videoPlayer;
